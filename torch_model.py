98b4a4edb2368724511a9414afaae166eb330a63
Kateryna <dubska.kate@gmail.com>
2019-08-17 22:11:29 +0300

    added progress bar + few fixes

--- torch_model.py
+++ torch_model.py
@@ -8,6 +8,8 @@ import torch.nn.functional as F
 import torchvision
 import torchvision.transforms as transforms
 
+from load_pics import load_jaffe, load_kanade
+
 class Net(nn.Module):
 
     def __init__(self, class_num, channel_num=1, batch_size=32):
@@ -77,12 +79,36 @@ class Net(nn.Module):
         return num_features
 
 
+# this is a progress bar function from 
+# https://stackoverflow.com/questions/3173320/text-progress-bar-in-the-console
+# Print iterations progress
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█'):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = '\r')
+    # Print New Line on Complete
+    if iteration == total: 
+        print()
+
+
 class Emotion_Net:
     def __init__(self, n_classes, im_channels, batch_size=32):
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self._model = Net(n_classes, im_channels, batch_size).to(self.device)
 
-    def train(self, trainloader, testloader, n_epochs=50, 
+    def train(self, trainloader, testloader, n_epochs=50, im_transposed=False,
                 optimizer='adam', save_best=False, save_best_path=''):
         criterion = nn.CrossEntropyLoss()
         opt = optim.SGD(self._model.parameters(), lr=0.001)
@@ -90,6 +116,8 @@ class Emotion_Net:
             opt = optim.Adam(self._model.parameters(), lr=0.005)
         best_accuracy = 0.0
         for epoch in range(n_epochs): 
+            l = len(trainloader)
+            printProgressBar(0, l, prefix = 'Epoch: ' + str(epoch + 1), suffix = 'Complete', length = 50)
             for i, data in enumerate(trainloader, 0):
                 inputs, labels = data[0].to(self.device), data[1].to(self.device)
                 
@@ -105,6 +133,7 @@ class Emotion_Net:
                 
                 #loss_trace.append(loss.item())
                 running_loss += loss.item()
+                printProgressBar(i + 1, l, prefix = 'Epoch: ' + str(epoch + 1), suffix = 'Complete', length = 50)
 
                 # if i % 100 == 99:  
                 #     print('[%d, %5d] loss: %.3f' %
@@ -116,7 +145,7 @@ class Emotion_Net:
             for data in testloader:
                 images, labels = data[0].to(self.device), data[1].to(self.device)
 
-                images = images.transpose(1, 3)
+                #images = images.transpose(1, 3)
 
                 outputs = self._model(images.float())
                 _, predicted = torch.max(outputs.data, 1)
@@ -135,3 +164,33 @@ class Emotion_Net:
     def load_model(self, path):
         self._model = torch.load(path)
                     
+
+#TODO: write data loaders
+def train_kanade_model(model_folder = 'models\\'):
+    """Trains a model based on kanade database and saves it's structure and weights"""
+    model_id = 'kanade_'
+    (im_rows, im_cols) = (490, 640)
+
+    #x_data, y_data = load_jaffe("project\\jaffe", 'tiff')
+    x_data, y_data = load_kanade("kanade\\cohn-kanade-images\\", "kanade\\emotion\\", (im_cols, im_rows))
+    n_classes = np.unique(y_data).shape[0]
+    #im_rows, im_cols = x_data.shape[1], x_data.shape[2]
+
+    input_shape = (im_rows, im_cols, 1)
+
+    # normalizing and preparing data
+    x_data = x_data.astype('float32')
+    x_data /= 255.0   
+    y_data = np_utils.to_categorical(y_data, n_classes)
+    x_data = x_data.reshape(x_data.shape[0], im_rows, im_cols, 1)
+    x_data = x_data.transpose(1, 3)
+    # x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.2, random_state=47)
+
+    # Model = Emotion_Net(n_classes, 1, 32)
+    # Model.train(x_train, y_train, x_test, y_test, save_best_path=model_folder, n_epochs=300, im_transposed=True)
+    # #Model.evaluate_accur(x_test, y_test)
+    # Model.save_model(model_folder + model_id + "model.json")
+    #Model.save_weights(model_folder + model_id + "model.h5")
+
+
+train_kanade_model()
