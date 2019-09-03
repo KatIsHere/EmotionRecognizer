19380e645d33142dd5afd65e7cc972f13d8e6761
yaaaola <43413443+yaaaola@users.noreply.github.com>
2019-09-03 18:06:20 +0300

    Update place_emoji

--- place_emoji.py
+++ place_emoji.py
@@ -2,11 +2,29 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 import copy
+import os
 
-# working with image
+def add_emoji_to_image(img, emoji, bounding_box): 
+    result = img.copy()
+    
+    emoji_png = emoji + '.png'
+    emoji_path = os.path.join('./emoji', emoji_png) 
+    emoji = cv2.imread(emoji_path, cv2.IMREAD_UNCHANGED)
+    
+    center = (bounding_box[0] + bounding_box[1]) // 2
+    
+    x0, y0 = bounding_box[0, 0], bounding_box[0, 1]
+    x1, y1 = bounding_box[1, 0], bounding_box[1, 1]
+    
+    size = y0 - y1
+    
+    emoji = cv2.resize(emoji, (size, size))
 
-emoji = cv2.imread('Smile.png', cv2.IMREAD_UNCHANGED)
-img = cv2.imread('Test.jpg', cv2.IMREAD_COLOR)
-cv2.imwrite('Test.png', img)
-img = cv2.imread('Test.png', cv2.IMREAD_UNCHANGED)
+    alpha_emoji = emoji[:, :, 3] / 255.0
+    alpha_emoji = np.array(alpha_emoji)
+    alpha_img = 1.0 - alpha_emoji
 
+    for c in range(0, 3):
+        result[y1:y0, y1:y0, c] = alpha_emoji * emoji[:, :, c] + alpha_img * img[y1:y0, y1:y0, c]
+
+    return result
